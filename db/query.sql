29e363329d974fa3c7f79d51d84a2006a00391d3
Tab-Y <tlyoumans@gmail.com>
2022-11-02 21:22:03 -0400

    set required modules

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,6 @@
 -- what info to be displayed
-SELECT
-FROM
-LEFT JOIN
+SELECT department.name AS department, role.department_id
+FROM role
+LEFT JOIN department
+ON role.department_id = department.id
+ORDER BY department.name;
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-// back end logic
+const express = require('express');
+const mysql = require('mysql2');
+const asciiart = require('asciiart-logo');
+const chalk = require('chalk');
+const table = require('console.table');
+const inquirer = require('inquirer');
