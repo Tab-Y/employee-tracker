568ee1a1ff2d6c2916fe3f3b8946b37e55e887b6
Tab-Y <tlyoumans@gmail.com>
2022-11-02 21:29:19 -0400

    setup mysql connection

--- server.js
+++ server.js
@@ -4,3 +4,21 @@ const asciiart = require('asciiart-logo');
 const chalk = require('chalk');
 const table = require('console.table');
 const inquirer = require('inquirer');
+const secretKey = require('./config/config.js');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.SECRETKEY || secretKey,
+        database: 'movies_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+);
